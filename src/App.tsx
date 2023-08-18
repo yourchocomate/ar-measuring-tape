8f378d00b2c80214d996a624d3bc0eed432dc1dc
yourchocomate <yourchocomate@gmail.com>
2023-08-18 22:51:57 +0600

    more experiments

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ArcRotateCamera, Vector3, HemisphericLight, Scene, SceneLoader, WebXRBackgroundRemover } from '@babylonjs/core';
+import { Vector3, HemisphericLight, Scene, SceneLoader, WebXRBackgroundRemover, WebXRHitTest, WebXRAnchorSystem, ArcRotateCamera } from '@babylonjs/core';
 import "@babylonjs/loaders/glTF";
 import './App.css'
 import * as BABYLON from "babylonjs-hook";
@@ -6,12 +6,31 @@ import * as BABYLON from "babylonjs-hook";
 function App() {
 
   const Baby = BABYLON.default;
-  const engine = BABYLON.useEngine();
-  engine?.setHardwareScalingLevel(0.5);
 
   const onSceneReady = async(scene: Scene) => {
 
+    // Get the canvas element from the DOM.
     const canvas = scene.getEngine().getRenderingCanvas();
+
+    // This creates and positions a free camera (non-mesh)
+    const camera = new ArcRotateCamera("camera", 0, 1.2, 10, Vector3.Zero(), scene);
+
+    // This targets the camera to scene origin
+    camera.setTarget(Vector3.Zero());
+  
+    // This attaches the camera to the canvas
+    camera.attachControl(canvas, true);
+  
+    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+    const light = new HemisphericLight("Hemi", new Vector3(0, 1, 0), scene);
+
+    // Default intensity is 1. Let's dim the light a small amount
+    light.intensity = 0.7;
+  
+    const result = await SceneLoader.ImportMeshAsync(null, "./models/", "Buggy.gltf", scene);
+    const buggy = result.meshes[0];
+    buggy.scaling.scaleInPlace(0.03);
+
     let xr;
     try {
       xr = await scene.createDefaultXRExperienceAsync({
@@ -30,24 +49,10 @@ function App() {
 
     const fm = xr?.baseExperience.featuresManager;
 
-    fm?.enableFeature(WebXRBackgroundRemover);
-
-    // This creates and positions a arc rotate camera
-    const camera = new ArcRotateCamera("Camera", 3, 1, 500, Vector3.Zero(), scene);
-  
-    // This attaches the camera to the canva
-    camera.attachControl(canvas, true);
-  
-    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    const light = new HemisphericLight("Hemi", new Vector3(0, 1, 0), scene);
-
-    light.intensity = 0.7;
-  
-    // Default intensity is 1. Let's dim the light a small amount
-    const result = await SceneLoader.ImportMeshAsync(null, "./models/", "Buggy.gltf", scene);
-    const buggy = result.meshes[0];
-    buggy.scaling = new Vector3(0.2,0.2,0.2);
-    camera.setTarget(buggy);
+    fm?.enableFeature(WebXRBackgroundRemover, "latest");
+    fm?.enableFeature(WebXRHitTest, "latest");
+    fm?.enableFeature(WebXRAnchorSystem, "latest");
+    // fm?.enableFeature(WebXRPlaneDetector, "latest");
   };
   
   /**
